3032c94eadb45c55b8da82f49532c051656cefee
hetasvi-patel <143477329+hetasvi-patel@users.noreply.github.com>
2024-04-05 08:17:08 +0530

    Create App.js

---
+++ useref/src/App.js
@@ -0,0 +1,13 @@
+import Refrence2 from "./Refrence2";
+
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Refrence /> */}
+    <Refrence2 />
+    </div>
+  );
+}
+
+export default App;
